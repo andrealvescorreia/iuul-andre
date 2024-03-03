ed15d7e98704d372e95132be6ee76263edbd60a2
Andre Alves Correia <andre2013aguabranca@gmail.com>
2024-03-02 22:36:00 -0300

    test: class GeraRelatorio

---
+++ tests/geraRelatorio.test.js
@@ -0,0 +1,34 @@
+const Vertice = require('../01/Vertice');
+const GeraRelatorio = require('../01/GeraRelatorio');
+
+test('gera relatorio 3 vertices', () => {
+  const v1 = new Vertice(0, 0);
+  const v2 = new Vertice(2, 3);
+  const v3 = new Vertice(2, 3);
+
+  const vertices = [v1, v2, v3];
+
+  const gerador = new GeraRelatorio(vertices);
+
+  const relatorioEsperado =
+    `v1 = (0,0)
+  v2 = (2,3)
+  v3 = (2,3)
+  ---
+  d(v1, v2) = 3.60555
+  d(v1, v3) = 3.60555
+  d(v2, v1) = 3.60555
+  d(v2, v3) = 0
+  d(v3, v1) = 3.60555
+  d(v3, v2) = 0
+  ---
+  v1 != v2
+  v1 != v3
+  v2 != v1
+  v2 == v3
+  v3 != v1
+  v3 == v2
+  `.replace(/  +/g, '');
+
+  expect(gerador.gera()).toBe(relatorioEsperado);
+});
