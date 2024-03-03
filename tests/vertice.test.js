ab8b53e8fd76e70c61fd6459416d20c22c5ecefd
Andre Alves Correia <andre2013aguabranca@gmail.com>
2024-03-02 22:34:49 -0300

    test: class Vertice

---
+++ tests/vertice.test.js
@@ -0,0 +1,46 @@
+const Vertice = require('../01/Vertice');
+
+
+test('cria novo vertice', () => {
+  const vertice = new Vertice(1, 3);
+  expect(vertice.x).toBe(1);
+  expect(vertice.y).toBe(3);
+});
+
+test('calcula distancia entre dois vertices iguais', () => {
+  const vertice1 = new Vertice(1, 3);
+  const vertice2 = new Vertice(1, 3);
+  expect(vertice1.distancia(vertice2)).toBe(0);
+});
+
+test('calcula distancia entre dois vertices', () => {
+  const vertice1 = new Vertice(1, 3);
+  const vertice2 = new Vertice(2, 5);
+  expect(vertice1.distancia(vertice2)).toBe(2.23607);
+});
+
+test('move vertice para 0,0', () => {
+  const vertice = new Vertice(1, 3);
+  vertice.move(0, 0)
+  expect(vertice.x).toBe(0);
+  expect(vertice.y).toBe(0);
+});
+
+test('move vertice', () => {
+  const vertice = new Vertice(1, 3);
+  vertice.move(10, 123)
+  expect(vertice.x).toBe(10);
+  expect(vertice.y).toBe(123);
+});
+
+test('verifica vertices iguais', () => {
+  const vertice1 = new Vertice(1, 3);
+  const vertice2 = new Vertice(1, 3);
+  expect(vertice1.equals(vertice2)).toBe(true);
+});
+
+test('verifica vertices diferentes', () => {
+  const vertice1 = new Vertice(1, 3);
+  const vertice2 = new Vertice(1, 4);
+  expect(vertice1.equals(vertice2)).toBe(false);
+});
