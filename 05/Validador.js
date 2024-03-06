b0217b6f1a9fd717bfa67d24cfb9595294f8a490
Andre Alves Correia <andre2013aguabranca@gmail.com>
2024-03-06 08:46:49 -0300

    style: ident code

--- 05/Validador.js
+++ 05/Validador.js
@@ -1,18 +1,18 @@
 module.exports = class Validador {
-  
-  validaNome(nome){
-    if(nome.length < 5) throw new Error('nome deve ter ao menos 5 caracteres');
+
+  validaNome(nome) {
+    if (nome.length < 5) throw new Error('nome deve ter ao menos 5 caracteres');
     return true;
   }
 
-  validaCPF(cpf){
-    if(cpf.length != 11) throw new Error('cpf deve ter 11 digitos');
-    if(!Number(cpf)) throw new Error('cpf deve ter valores numericos apenas');
+  validaCPF(cpf) {
+    if (cpf.length != 11) throw new Error('cpf deve ter 11 digitos');
+    if (!Number(cpf)) throw new Error('cpf deve ter valores numericos apenas');
 
     return true;
   }
 
-  validaData(data){
+  validaData(data) {
     throw new Error('data deve estar no formato DD/MM/AAAA');
     return true;
   }
--- tests/Validador.test.js
+++ tests/Validador.test.js
@@ -7,39 +7,38 @@ test('nome valido', () => {
 
 });
 
-test('nome invalido', () => { 
- expect(()=>validador.validaNome('An')).toThrow('nome deve ter ao menos 5 caracteres');
+test('nome invalido', () => {
+  expect(() => validador.validaNome('An')).toThrow('nome deve ter ao menos 5 caracteres');
 });
 
 test('cpf valido', () => {
- expect(validador.validaCPF('12345678910')).toBe(true);
+  expect(validador.validaCPF('12345678910')).toBe(true);
 })
 
-test('deve lancar erro cpf que nao tem 11 digitos',()=>{
-  expect(()=>validador.validaCPF('123')).toThrow('cpf deve ter 11 digitos');
+test('deve lancar erro cpf que nao tem 11 digitos', () => {
+  expect(() => validador.validaCPF('123')).toThrow('cpf deve ter 11 digitos');
 
-  expect(()=> validador.validaCPF('123456789101')).toThrow('cpf deve ter 11 digitos');
+  expect(() => validador.validaCPF('123456789101')).toThrow('cpf deve ter 11 digitos');
 });
 
-test('deve lancar erro cpf nao numerico',()=>{
-  expect(()=> validador.validaCPF('a2345678910')).toThrow('cpf deve ter valores numericos apenas')
+test('deve lancar erro cpf nao numerico', () => {
+  expect(() => validador.validaCPF('a2345678910')).toThrow('cpf deve ter valores numericos apenas')
 })
 
-test('data valida',()=>{
+test('data valida', () => {
   expect(validador.validaData('01/01/0001')).toBe(true);
-
-  expect(validador.validaData('31/12/9999')).toBe(true);
+  expect(validador.validaData('06/03/2024')).toBe(true);
   expect(validador.validaData('31/12/9999')).toBe(true);
 })
 
-test ('data invalida',()=>{
-  expect(()=>validador.validaData('data')).toThrow('data deve estar no formato DD/MM/AAAA');
+test('data invalida', () => {
+  expect(() => validador.validaData('data')).toThrow('data deve estar no formato DD/MM/AAAA');
 
-  expect(()=>validador.validaData('100/01/1')).toThrow('data deve estar no formato DD/MM/AAAA');
+  expect(() => validador.validaData('100/01/1')).toThrow('data deve estar no formato DD/MM/AAAA');
 
 
-  expect(()=>validador.validaData('99/99/9999')).toThrow('data deve estar no formato DD/MM/AAAA');
+  expect(() => validador.validaData('99/99/9999')).toThrow('data deve estar no formato DD/MM/AAAA');
 
-  
-  expect(()=>validador.validaData('12/12/12')).toThrow('data deve estar no formato DD/MM/AAAA');
+
+  expect(() => validador.validaData('12/12/12')).toThrow('data deve estar no formato DD/MM/AAAA');
 })
