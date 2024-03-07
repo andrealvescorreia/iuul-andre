95b8fb729bf238e970a7d4ec2b0b92a61cacbe07
Andre Alves Correia <andre2013aguabranca@gmail.com>
2024-03-06 23:20:01 -0300

    fix: catch error and try again

--- 02/LeitorTriangulos.js
+++ 02/LeitorTriangulos.js
@@ -5,11 +5,17 @@ const LeitorVertices = require('../01/LeitorVertices');
 module.exports = class LeitorTriangulos {
   lê(qtdTriangulos = 3) {
     let triangulos = [];
-    console.log(`A seguir, informe os vertices de ${qtdTriangulos} triangulos\n`)
+    console.log(`A seguir, informe os vertices de ${qtdTriangulos} triangulos`)
     for (let i = 0; i < qtdTriangulos; i++) {
-      console.log(`Informe os vertices do ${i + 1}° Triangulo :`)
-      const verticesTriangulo = LeitorVertices.lê(3);
-      triangulos.push(new Triangulo(verticesTriangulo));
+      try {
+        console.log(`\nInforme os vertices do ${i + 1}° Triangulo :`)
+        const verticesTriangulo = LeitorVertices.lê(3);
+        triangulos.push(new Triangulo(verticesTriangulo));
+      } catch (error) {
+        console.log(error.message);
+        console.log('tente novamente.')
+        i--;
+      }
     }
     return triangulos;
   }
