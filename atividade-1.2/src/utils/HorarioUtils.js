31216618c52c3b987c7fcbcf706b229b3567c9aa
Andre Alves Correia <andre2013aguabranca@gmail.com>
2024-03-13 11:33:42 -0300

    fix: add type check string

--- atividade-1.2/src/utils/HorarioUtils.js
+++ atividade-1.2/src/utils/HorarioUtils.js
@@ -2,6 +2,7 @@ const HorarioValidator = require('../validators/HorarioValidator');
 
 module.exports = class HorarioUtils {
   static horarioToDate(horarioStr) {
+    if (typeof horarioStr !== 'string') { throw new Error(`horário deve ser string mas recebeu ${typeof horarioStr}`); }
     if (!HorarioValidator.valido(horarioStr)) throw new Error('horário com formato inválido');
     return new Date(`1/1/1970 ${horarioStr.slice(0, 2)}:${horarioStr.slice(-2)}:00`);
   }
--- atividade-1.2/src/validators/HorarioValidator.js
+++ atividade-1.2/src/validators/HorarioValidator.js
@@ -1,5 +1,6 @@
 module.exports = class HorarioValidator {
   static valido(horarioStr) {
+    if (typeof horarioStr !== 'string') return false;
     const horaRegex = /^(0[0-9]|1[0-9]|2[0-3])[0-5][0-9]$/;
     const match = horarioStr.match(horaRegex);
     if (!match) return false;
