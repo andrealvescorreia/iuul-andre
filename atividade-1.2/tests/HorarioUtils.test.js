16f35ad5307f71bb8cf043b766b6ab1468f12ce8
Andre Alves Correia <andre2013aguabranca@gmail.com>
2024-03-13 12:06:56 -0300

    test: HorarioUtils.seSobrepoem()

--- atividade-1.2/tests/HorarioUtils.test.js
+++ atividade-1.2/tests/HorarioUtils.test.js
@@ -15,3 +15,37 @@ test('horario fora do limite', () => {
   expect(HorarioUtils.dentroDoLimite('0800', '1900', '1900')).toBe(false);
   expect(HorarioUtils.dentroDoLimite('0800', '1900', '2330')).toBe(false);
 });
+
+test('horarios não se sobrepoem', () => {
+  expect(HorarioUtils.seSobrepoem(
+    { inicio: '0800', fim: '1900' },
+    { inicio: '1900', fim: '2000' },
+  )).toBe(false);
+
+  expect(HorarioUtils.seSobrepoem(
+    { inicio: '1900', fim: '2000' },
+    { inicio: '0800', fim: '1900' },
+  )).toBe(false);
+
+  expect(HorarioUtils.seSobrepoem(
+    { inicio: '1000', fim: '1030' },
+    { inicio: '1035', fim: '1040' },
+  )).toBe(false);
+});
+
+test('horarios se sobrepoem', () => {
+  expect(HorarioUtils.seSobrepoem(
+    { inicio: '0800', fim: '1900' },
+    { inicio: '0800', fim: '1900' },
+  )).toBe(true);
+
+  expect(HorarioUtils.seSobrepoem(
+    { inicio: '0800', fim: '1900' },
+    { inicio: '0759', fim: '1901' },
+  )).toBe(true);
+
+  expect(HorarioUtils.seSobrepoem(
+    { inicio: '1000', fim: '1030' },
+    { inicio: '1029', fim: '1040' },
+  )).toBe(true);
+});
