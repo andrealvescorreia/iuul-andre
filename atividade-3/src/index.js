947d4ddfc0e4267c70a2bfa67bb1b95cf2342a76
André Alves Correia <andre2013aguabranca@gmail.com>
2024-04-24 20:39:57 -0300

    refactor: remove unused models

--- atividade-3/src/index.js
+++ atividade-3/src/index.js
@@ -1,15 +1,9 @@
 const dotenv = require('dotenv');
-const Aluno = require('./sequelize/models/Aluno.js');
 const database = require('./sequelize/database.js');
-const Universidade = require('./sequelize/models/Universidade.js');
 
 dotenv.config();
-// const inicia = require('./sequelize/index.js');// auto executa
 
 (async () => {
-  // await inicia();
   await database.authenticate(); // assegura que a conexão foi bem sucedida
   await database.sync({ force: true });
-  const uni = await Universidade.create({ nome: 'UEPB' });
-  const alu = await Aluno.create({ nome: 'maria', idUniversidade: uni.id });
 })();
