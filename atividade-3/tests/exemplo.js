006b3e2dc6e338dd25fc8346ccd60a51036e6898
André Alves Correia <andre2013aguabranca@gmail.com>
2024-04-24 20:40:17 -0300

    chore: exemplo de uso dos models

---
+++ atividade-3/tests/exemplo.js
@@ -0,0 +1,32 @@
+const dotenv = require('dotenv');
+const Paciente = require('../src/sequelize/models/Paciente.js');
+const database = require('../src/sequelize/database.js');
+const Agendamento = require('../src/sequelize/models/Agendamento.js');
+
+dotenv.config();
+
+(async () => {
+  await database.sync({ force: true });
+  try {
+    /*
+      const p = await Paciente.create({
+      cpf: '14386861459',
+      nome: 'andree',
+      dataNascimento: '2022-05-10'
+    }); */
+    const p = await Paciente.create({ cpf: '14386861459', nome: 'andree', dataNascimento: '2002-05-10' });
+    await Agendamento.create({
+      dataHoraInicio: new Date('2010-12-17T07:00:00'),
+      dataHoraFim: new Date('2010-12-17T11:00:00'),
+      cpfPaciente: p.cpf,
+    });
+    await Agendamento.create({
+      dataHoraInicio: new Date('2010-12-17T11:00:00'),
+      dataHoraFim: new Date('2010-12-17T14:45:00'),
+      cpfPaciente: p.cpf,
+    });
+    console.log('deu nenhum erro');
+  } catch (e) {
+    console.log(e.message);
+  }
+})();
