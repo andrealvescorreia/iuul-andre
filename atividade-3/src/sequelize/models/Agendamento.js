0f23a0baf8278a8f813f70e4b3a27508c0670ce0
André Alves Correia <andre2013aguabranca@gmail.com>
2024-04-26 13:54:27 -0300

    refactor: create function haConsultaAgendada()

--- atividade-3/src/sequelize/models/Agendamento.js
+++ atividade-3/src/sequelize/models/Agendamento.js
@@ -15,25 +15,12 @@ function validaDataHora(dateTime) {
   ) {
     throw new Error('não obdece bloco de 15 minutos');
   }
-
   if (
     dateTime.hour < 8
     || dateTime.hour > 19
     || (dateTime.hour === 19 && dateTime.minute > 0)
   ) {
     throw new Error('fora do horario de funcionamento');
-  }
-}
-
-function validaHorarios(dataHoraInicio, dataHoraFim) {
-  if (dataHoraFim === dataHoraInicio) {
-    throw new Error('horário fim não pode ser igual a horário início');
-  }
-  if (dataHoraFim < dataHoraInicio) {
-    throw new Error('horário fim não pode anteceder horário início');
-  }
-  if (dataHoraInicio.toISODate() !== dataHoraFim.toISODate()) {
-    throw new Error('horário inicio e fim devem estar no mesmo dia');
   }
 }
 
@@ -45,6 +32,41 @@ const hasIntersecaoHorario = (dataHoraInicio1, dataHoraFim1, dataHoraInicio2, da
   || (dataHoraInicio1 <= dataHoraInicio2
     && dataHoraFim1 >= dataHoraFim2
   );
+
+async function haConsultaAgendada(periodoInicio, periodoFim) {
+  const dataAgendamento = periodoInicio.toISODate();
+  const agendamentosDoDia = await database.query(`SELECT * FROM agendamentos WHERE CAST(data_hora_inicio AS DATE) = '${dataAgendamento}'`, {
+    type: QueryTypes.SELECT,
+  });
+
+  let temAgendamento = false;
+  agendamentosDoDia.forEach((agendamentoDoDia) => {
+    if (hasIntersecaoHorario(
+      agendamentoDoDia.data_hora_inicio,
+      agendamentoDoDia.data_hora_fim,
+      periodoInicio,
+      periodoFim,
+    )) {
+      temAgendamento = true;
+    }
+  });
+  return temAgendamento;
+}
+
+async function validaHorarios(dataHoraInicio, dataHoraFim) {
+  if (dataHoraFim === dataHoraInicio) {
+    throw new Error('horário fim não pode ser igual a horário início');
+  }
+  if (dataHoraFim < dataHoraInicio) {
+    throw new Error('horário fim não pode anteceder horário início');
+  }
+  if (dataHoraInicio.toISODate() !== dataHoraFim.toISODate()) {
+    throw new Error('horário inicio e fim devem estar no mesmo dia');
+  }
+  if (await haConsultaAgendada(dataHoraInicio, dataHoraFim)) {
+    throw new Error('há conflito com outro horario agendado!');
+  }
+}
 
 const Agendamento = database.define(
   'Agendamento',
@@ -72,23 +94,7 @@ const Agendamento = database.define(
           const dateTimeInicio = DateTime.fromISO(this.dataHoraInicio.toISOString());
           const dateTimeFim = DateTime.fromISO(dataHoraFim.toISOString());
           validaDataHora(dateTimeFim);
-          validaHorarios(dateTimeInicio, dateTimeFim);
-
-          const dataAgendamento = this.dataHoraInicio.toISOString().substr(0, 10);
-          const agendamentosDoDia = await database.query(`SELECT * FROM agendamentos WHERE CAST(data_hora_inicio AS DATE) = '${dataAgendamento}'`, {
-            type: QueryTypes.SELECT,
-          });
-
-          agendamentosDoDia.forEach((agendamentoDoDia) => {
-            if (hasIntersecaoHorario(
-              agendamentoDoDia.data_hora_inicio,
-              agendamentoDoDia.data_hora_fim,
-              this.dataHoraInicio,
-              this.dataHoraFim,
-            )) {
-              throw new Error('há conflito com outro horario agendado!');
-            }
-          });
+          await validaHorarios(dateTimeInicio, dateTimeFim);
         },
       },
     },
