c8b06e6ea25c8062d576f5a7a4b2b55107fef285
André Alves Correia <andre2013aguabranca@gmail.com>
2024-04-26 13:29:35 -0300

    chore: update example

--- atividade-3/tests/exemplo.js
+++ atividade-3/tests/exemplo.js
@@ -10,13 +10,13 @@ dotenv.config();
   try {
     const p = await Paciente.create({ cpf: '14386861459', nome: 'andree', dataNascimento: '2002-05-10' });
     await Agendamento.create({
-      dataHoraInicio: new Date('2010-12-17T07:00:00'), // aqui está fora do horario de funcionamento
-      dataHoraFim: new Date('2010-12-17T11:00:00'),
+      dataHoraInicio: new Date('2024-12-17T08:00:00'),
+      dataHoraFim: new Date('2024-12-17T11:00:00'),
       cpfPaciente: p.cpf,
     });
     await Agendamento.create({
-      dataHoraInicio: new Date('2010-12-17T11:00:00'),
-      dataHoraFim: new Date('2010-12-17T14:45:00'),
+      dataHoraInicio: new Date('2024-12-17T11:00:00'),
+      dataHoraFim: new Date('2024-12-17T14:45:00'),
       cpfPaciente: p.cpf,
     });
     console.log('deu nenhum erro');
